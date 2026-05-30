f5b8609a0c78a23901276549816f4882e9677ace
Michał <michal.taglewski@wp.pl>
2026-05-30 13:51:18 +0200

    Add English and Polish language files for authentication, validation, and pagination messages

---
+++ api/lang/pl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Wprowadzone dane są nieprawidłowe.',
+    'password' => 'Wprowadzone hasło jest nieprawidłowe',
+    'throttle' => 'Zbyt wiele prób. Spróbuj ponownie za :seconds sekund.',
+    'logged_out' => 'Zostałeś pomyślnie wylogowany',
+];
